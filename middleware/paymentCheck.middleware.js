1e51da40803606ba8d627adc809636542db6ff2c
panjasaibal <panjasaibal1999@gmail.com>
2024-03-19 10:53:40 +0530

    fixed payment middleware for admin login

--- middleware/paymentCheck.middleware.js
+++ middleware/paymentCheck.middleware.js
@@ -2,8 +2,9 @@ const Admin = require('../model/adminstration');
 
 const paymentStatus = async(req,res,next)=>{
 
-    let currentAdmin = Admin.findById(req.body.admin);
+    let currentAdmin = await Admin.findOne({email:req.body.email, passwd:req.body.passwd});
     if(!currentAdmin.access){
+        console.log(currentAdmin.access)
         return res.status(402).send({message:"Please clear you dues to resume services"});
     }
         next();
