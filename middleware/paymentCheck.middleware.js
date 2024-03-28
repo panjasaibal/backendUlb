ebc5dc8dcc5ae27ebef1f8a99ee0cac56074965b
panjasaibal <panjasaibal1999@gmail.com>
2024-03-28 21:29:03 +0530

    added put api for admin access

--- middleware/paymentCheck.middleware.js
+++ middleware/paymentCheck.middleware.js
@@ -4,6 +4,7 @@ const paymentStatus = async(req,res,next)=>{
 
     let currentAdmin = await Admin.findOne({email:req.body.email, passwd:req.body.passwd});
     if(!currentAdmin.access){
+        console.log(currentAdmin)
         console.log(currentAdmin.access)
         return res.status(402).send({message:"Please clear you dues to resume services"});
     }
--- routers/superAdmin.route.js
+++ routers/superAdmin.route.js
@@ -1,108 +1,149 @@
-const express = require('express');
-const {body,validationResult} = require('express-validator');
-const SuperAdmin = require('../model/superadmin.model');
-const Adminstration = require('../model/adminstration');
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const SuperAdmin = require("../model/superadmin.model");
+const Adminstration = require("../model/adminstration");
 
+const fetchSuperAdmin = require("../middleware/superadmin.middlware");
 
 const router = express.Router();
 
 //create Admin "/"
-router.post('/',[
-    body('username','Please enter a valid name').isLength({min:3}),
-    body('password','Password should be atleast 4 charecters').isLength({min:4}),
-
-], async(req, res)=>{
+router.post(
+  "/",
+  [
+    body("username", "Please enter a valid name").isLength({ min: 3 }),
+    body("password", "Password should be atleast 4 charecters").isLength({
+      min: 4,
+    }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
     try {
-        let superAdminUser = await SuperAdmin.findOne({name:req.body.username});
-        if(superAdminUser){
-            return res.status(400).json({error:'admin already exists'})
-        }
-        superAdminUser = await SuperAdmin.create({
-            username:req.body.username,
-            password:req.body.password
-        }) 
+      let superAdminUser = await SuperAdmin.findOne({
+        name: req.body.username,
+      });
+      if (superAdminUser) {
+        return res.status(400).json({ error: "admin already exists" });
+      }
+      superAdminUser = await SuperAdmin.create({
+        username: req.body.username,
+        password: req.body.password,
+      });
 
-        const data = {superAdminUser:{id:superAdminUser._id}}
-        res.send(data);
-
-    }catch(error){
-        console.error(error.message); 
-       res.status(500).send('Internal server error')
+      const data = { superAdminUser: { id: superAdminUser._id } };
+      res.json(data);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
     }
-});
+  }
+);
 
 //login superadmin /loginsuperadmin
-router.post('/loginsuperadmin',[
-    body('name','Please enter a valid email').isLength({min:5}),
-    body('password','Password should be atleast 4 charecters').isLength({min:4}),
-
-], async (req, res)=>{
+router.post(
+  "/loginsuperadmin",
+  [
+    body("name", "Please enter a valid email").isLength({ min: 5 }),
+    body("password", "Password should be atleast 4 charecters").isLength({
+      min: 4,
+    }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
-    }
-    try{
-        let superadminUser = await SuperAdmin.findOne({name:req.body.name});
-        if(superadminUser===null || superadminUser.password!==req.body.password){
-            return res.status(400).json({'error':"Incorrect email or password", success:false});
-        }
-        const data = {'id':superadminUser._id,'name':superadminUser.name, success:true};
-        res.send(data);
-
-    }catch(error){
-        console.error(error.message); 
-        res.status(500).send('Internal server error');
-    }
-})
-
-router.get('/get', (req, res)=>{
-        res.json({'res':'Hello i am api'})
-})
-
-//create admin /createadmin
-router.post('/createadmin',[
-    body('superadmin'),
-    body('name','Please enter a valid name').isLength({min:3}),
-    body('email','Please enter a valid email').isEmail(),
-    body('passwd','Password should be atleast 4 charecters').isLength({min:4}),
-
-], async(req, res)=>{
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
     try {
-        let superadmin = await SuperAdmin.findById(req.body.superadmin);
-        if(!superadmin){
-           return res.status(500).send("Access Denied");
-        }
-        let adminUser = await Adminstration.findOne({email:req.body.email});
-        if(adminUser){
-            return res.status(400).json({error:'email id already exists'})
-        }
-        adminUser = await Adminstration.create({
-            superadmin:req.body.superadmin,
-            name:req.body.name,
-            email:req.body.email,
-            passwd:req.body.passwd
-        }) 
-
-        const data = {adminUser:{id:adminUser._id}}
-        res.send(data);
-
-    }catch(error){
-        console.error(error.message); 
-       res.status(500).send('Internal server error')
-
+      let superadminUser = await SuperAdmin.findOne({ name: req.body.name });
+      if (
+        superadminUser === null ||
+        superadminUser.password !== req.body.password
+      ) {
+        return res
+          .status(400)
+          .json({ error: "Incorrect email or password", success: false });
+      }
+      const data = {
+        id: superadminUser._id,
+        name: superadminUser.name,
+        success: true,
+      };
+      res.json(data);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
     }
-    
-})
+  }
+);
+
+router.get("/get", (req, res) => {
+  res.json({ res: "Hello i am api" });
+});
+
+//create admin /createadmin
+router.post(
+  "/createadmin",
+  [
+    body("superadmin"),
+    body("name", "Please enter a valid name").isLength({ min: 3 }),
+    body("email", "Please enter a valid email").isEmail(),
+    body("passwd", "Password should be atleast 4 charecters").isLength({
+      min: 4,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      let superadmin = await SuperAdmin.findById(req.body.superadmin);
+      if (!superadmin) {
+        return res.status(500).send("Access Denied");
+      }
+      let adminUser = await Adminstration.findOne({ email: req.body.email });
+      if (adminUser) {
+        return res.status(400).json({ error: "email id already exists" });
+      }
+      adminUser = await Adminstration.create({
+        superadmin: req.body.superadmin,
+        name: req.body.name,
+        email: req.body.email,
+        passwd: req.body.passwd,
+      });
+
+      const data = { adminUser: { id: adminUser._id } };
+      res.json(data);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+router.put("/admin/:id", fetchSuperAdmin, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { access } = req.body;
+    let admin = await Adminstration.findById(id);
+    if (admin == null) {
+      return res.status(401).send("Access Denied");
+    }
+    admin = await Adminstration.findByIdAndUpdate(
+      id,
+      { $set: { access: access } },
+      { new: true }
+    );
+    res.json(admin);
+  } catch (error) {
+    res.status(500).send("something went wrong");
+  }
+});
 
 module.exports = router;
