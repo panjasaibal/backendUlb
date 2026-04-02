ecf1cc1ee65133f5fc092fe40e1aaea876d66908
panjasaibal <panjasaibal1999@gmail.com>
2026-04-03 01:44:28 +0530

    js file cleanups

--- src/middleware/multer.middleware.ts
+++ src/middleware/multer.middleware.ts
@@ -1,16 +1,18 @@
-const multer = require("multer");
-const path = require('path')
+import multer from "multer";
+import path from 'path';
+
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (_req, _file, cb) {
 
     cb(null, "./public/uploads"); // Destination folder for uploads
   },
-  filename: function (req, file, cb) {
+  filename: function (_req , file, cb) {
    
     cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname); // Naming convention for the uploaded files
   }
 });
 
-const upload = multer({storage})
+const upload = multer({storage});
 
-module.exports = upload
+export { upload };
